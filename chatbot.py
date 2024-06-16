546e03fcf05ef0628093b07c00333d5513cdafa6
oxorudo <87151398+oxorudo@users.noreply.github.com>
2024-06-16 18:04:07 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,10 @@
 from common import client, makeup_response
 import math
 from memory_manager import MemoryManager
+from function_calling import FunctionCalling, func_specs
+from common import model, currTime
+
+func_calling = FunctionCalling(model=model.basic)
 
 class Chatbot:
     
@@ -16,32 +20,38 @@ class Chatbot:
         self.context.extend(self.memoryManager.restore_chat())
 
     def add_user_message(self, user_message):
-        self.context.append({"role": "user", "content": user_message, "saved" : False})
+        self.context.append({"role": "user", "content": user_message, "saved":False})
 
-    def _send_request(self):
-        try:
-            response = client.chat.completions.create(
-                model=self.model, 
-                messages=self.to_openai_contenxt(),
-                temperature=0.5,
-                top_p=1,
-                max_tokens=256,
-                frequency_penalty=0,
-                presence_penalty=0
-            ).model_dump()
-        except Exception as e:
-            print(f"Exception 오류({type(e)}) 발생:{e}")
-            if 'maximum context length' in str(e):
-                self.context.pop()
-                return makeup_response("메시지 조금 짧게 보내줄래?")
-            else: 
-                return makeup_response("[내 찐친 챗봇에 문제가 발생했습니다. 잠시 뒤 이용해주세요]")
 
-        return response
     
+    def _send_request(self):
+        analyzed_dict = func_calling.analyze(self.context[-1]['content'], func_specs) # self.context[-1]['content'] = user_message
+        if analyzed_dict.get("function_call"): # 단일 함수 호출
+            response = func_calling.run(analyzed_dict, self.context[:]) # 단일 함수 호출
+        else:
+            try:
+                response = client.chat.completions.create(
+                    model=self.model, 
+                    messages=self.to_openai_contenxt(),
+                    temperature=0.5,
+                    top_p=1,
+                    max_tokens=256,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                ).model_dump()
+            except Exception as e:
+                print(f"Exception 오류({type(e)}) 발생:{e}")
+                if 'maximum context length' in str(e):
+                    self.context.pop()
+                    return makeup_response("메시지 조금 짧게 보내줄래?")
+                else:
+                    return makeup_response("[내 찐친 챗봇에 문제가 발생했습니다. 잠시 뒤 이용해주세요]")
+        return response
+
     def send_request(self):
         self.context[-1]['content'] += self.instruction
-        return self._send_request()        
+        return self._send_request()
+    
 
     def add_response(self, response):
         response_message = {
